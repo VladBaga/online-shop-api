eb2375a04da97bcc0e0cb16e4e17e47c17f03a19
VladBaga <dalvutz.k3lu@yahoo,com>
2019-04-04 21:31:26 +0300

    studied custom exceptions; studied SpringBoot - CRUD Service Example; updated config

--- src/test/java/org/fasttrackit/onlineshopapi/ProductServiceIntegrationTests.java
+++ src/test/java/org/fasttrackit/onlineshopapi/ProductServiceIntegrationTests.java
@@ -1,9 +1,12 @@
 package org.fasttrackit.onlineshopapi;
 
 import org.fasttrackit.onlineshopapi.domain.Product;
+import org.fasttrackit.onlineshopapi.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshopapi.service.ProductService;
 import org.fasttrackit.onlineshopapi.transfer.CreateProductRequest;
+import org.fasttrackit.onlineshopapi.transfer.UpdateProductRequest;
 import org.hamcrest.core.IsNull;
+import org.hibernate.sql.Update;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,16 +25,57 @@ public class ProductServiceIntegrationTests {
     private ProductService productService;
 
     @Test
-    public void testCreateProduct_whenValidRequest_thenReturnProductWithId(){
+    public void testCreateProduct_whenValidRequest_thenReturnProductWithId(){ //metodele pt test intotdeauna VOID
+        Product product = createProduct();
+
+        assertThat(product, notNullValue());
+        assertThat(product.getId(), greaterThan(0L));
+    }
+
+    private Product createProduct() {
         CreateProductRequest request = new CreateProductRequest();
         request.setName("Laptop");
         request.setPrice(10);
         request.setQuantity(3);
         request.setSku("asdfghjkl");
 
-        Product product = productService.createProduct(request);
+        return productService.createProduct(request);
+    }
 
-        assertThat(product, notNullValue());
-        assertThat(product.getId(), greaterThan(0L));
+    @Test (expected = ResourceNotFoundException.class)
+    public void testGetProduct_whenProductNotFound_thenThrowResourceNotFoundException() throws ResourceNotFoundException {
+        productService.getProduct(0);
+    }
+
+    @Test
+    public void testUpdateProduct_whenValidRequestWithAllFields_thenReturnUpdatedProduct() throws ResourceNotFoundException { //ORICE TEST ESTE INDEPENDENT
+        Product createdProduct = createProduct();
+
+        UpdateProductRequest request = new UpdateProductRequest();
+        request.setName(createdProduct.getName() + " Edited");
+        request.setPrice(createdProduct.getPrice() + 10);
+        request.setQuantity(createdProduct.getQuantity() + 5);
+        request.setSku(createdProduct.getSku() + "Edited");
+
+        Product updatedProduct = productService.updateProduct(createdProduct.getId(), request);
+
+        assertThat(updatedProduct.getName(), is(request.getName()));
+        assertThat(updatedProduct.getName(), not(is(createdProduct.getName())));
+
+        assertThat(updatedProduct.getPrice(), is(request.getPrice()));
+        assertThat(updatedProduct.getQuantity(), is(request.getQuantity()));
+        assertThat(updatedProduct.getSku(), is(request.getSku()));
+
+        assertThat(updatedProduct.getId(), is(createdProduct.getId()));
+    }
+    // todo: Implement negative tests for update and tests for update with some fields only.
+
+    @Test (expected = ResourceNotFoundException.class)
+    public void testDeleteProduct_whenExistingId_thenProductIsDeleted() throws ResourceNotFoundException {
+     Product createdProduct = createProduct();
+
+     productService.deleteProduct(createdProduct.getId());
+
+     productService.getProduct(createdProduct.getId());
     }
 }
